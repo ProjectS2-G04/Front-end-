7eb5081a5e2bb276989512d36b5bcce538cc1be9
nessrine <n.mehda@esi-sba.dz>
2025-04-05 13:19:10 +0100

    Save local changes before pulling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPage from './components/Admin_Page';
 import Forgot_Password from './components/Forgot_Password';
 import Forgot_Password1 from './components/Forgot_Password1';
@@ -9,39 +9,25 @@ import Profile from "./components/profile";
 import SeConnecter from "./components/seConnecter";
 import Signup_User from './components/Signup_User';
 import Sinscrire from "./components/sinscrire";
-import SideBareDocs from './components/SideBareDocs';
-import DoctorList from './components/DoctorList';
 import AssitantHome from './components/AssitantHome';
-import PatientHome from './components/PatientHome';
-import AssistantList from './components/AssistantList';
 
 const App = () => {
   return (
-  <>
-  
-    {/* <DoctorList/>  
-      <AssitantHome/>
-     <PatientHome/>
-      <AssistantList/>
-    */ } 
-     <Router> 
-    <Routes>
-      <Route path="/seConnecter" element={<SeConnecter />} /> 
-      <Route path="/home" element={<Home />} /> 
-      <Route path="/profile" element={<Profile />} /> 
-      <Route path="/sinscrire" element={<Sinscrire />} /> 
-      <Route path="/forgot-password" element={<Forgot_Password />} />
-      <Route path="/enter-code" element ={<Forgot_Password1/>} />
-      <Route path="/reset-password" element ={<Forgot_Password2/>}/>
-      <Route path="/" element={<AdminPage />} />
-      <Route path="/signup_user" element={<Signup_User />} />
-      <Route path="/Assitanthome" element={<AssitantHome />} />
-    </Routes>
-  </Router>
-  
-  
-  </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<SeConnecter />} />
+        <Route path="/home" element={<Doctorhome />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sinscrire" element={<Sinscrire />} />
+        <Route path="/forgot-password" element={<Forgot_Password />} />
+        <Route path="/enter-code" element={<Forgot_Password1 />} />
+        <Route path="/reset-password" element={<Forgot_Password2 />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/signup_user" element={<Signup_User />} />
+        <Route path="/assistant-home" element={<AssitantHome />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
