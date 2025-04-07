ac9cafbce23d4eabef0f32ef1751195fe1f12e70
belhouarikhadhra <belhouarikhadhra@gmail.com>
2025-04-07 18:12:08 +0200

    fixed home patient navigation

--- src/components/PatientHome.jsx
+++ src/components/PatientHome.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './home.css'
 import homeLogo from "../assets/logo.png"
 import homeUser from "../assets/user.png"
+import { useNavigate } from 'react-router-dom'
 import { HiMiniDocumentText } from "react-icons/hi2";
 import OnlineDoctoramico from "/src/assets/OnlineDoctor-amico.svg";
 function PatientHome() {
+  const navigate = useNavigate();
   return (
    <div className='home-container'>
        
@@ -12,9 +14,9 @@ function PatientHome() {
                    <img src={homeLogo} alt="Medeciel Logo" />
                 <nav>
                   
-                <img src={homeUser} alt="Profile Icon" />
+          <img src={homeUser} alt="Profile Icon" onClick={() => navigate("/profile")} />
                 
-                 <button className="deconnexion-button">Déconnexion</button>
+          <button className="deconnexion-button" onClick={() => navigate("/")}>Déconnexion</button>
                 
                 </nav>
            </heder>
